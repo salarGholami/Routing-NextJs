0b844b5246c59f85086a17cd6bc32451e55dbd94
salarGholami <salargholami021@gmail.com>
2024-11-24 14:30:56 +0330

    NotFound page

---
+++ src/app/products/[productId]/reviews/[reviewId]/not-found.js
@@ -0,0 +1,5 @@
+function NotFound() {
+  return <div>NotFound review Page</div>;
+}
+
+export default NotFound;
--- src/app/products/[productId]/reviews/[reviewId]/page.js
+++ src/app/products/[productId]/reviews/[reviewId]/page.js
@@ -1,4 +1,10 @@
+import { notFound } from "next/navigation";
+
 function ReviewDetail({ params }) {
+  if (parseInt(params.reviewId) > 100) {
+    notFound();
+  }
+  
   return (
     <div>
       Review {params.reviewId} for product {params.productId}

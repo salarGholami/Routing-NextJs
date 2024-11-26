13c554eb461b9dd399d81f568bd4aaf8b3c5d0a9
salarGholami <salargholami021@gmail.com>
2024-11-26 14:33:52 +0330

    dynamic metadata

--- src/app/(user)/products/[productId]/page.js
+++ src/app/(user)/products/[productId]/page.js
@@ -1,3 +1,20 @@
+export function generateMetadata({ params }) {
+  const id = params.productId;
+
+  // const product = await fetch(`https://.../${id}`).then((res) => res.json())
+
+  const title = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(`t-shirt ${id}`);
+    }, 100);
+  });
+
+  return {
+    title: title,
+    description: "demo desc ...",
+  };
+}
+
 function ProductDetailPage({ params }) {
   return <div>ProductDetailPage - {params.productId}</div>;
 }

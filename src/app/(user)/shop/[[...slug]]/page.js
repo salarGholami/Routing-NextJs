4415ad2fc2983b3b0c85fd8ceef9dd1cf4313447
salarGholami <salargholami021@gmail.com>
2024-11-27 16:42:50 +0330

    Programmatically Navigate

--- src/app/(user)/shop/[[...slug]]/page.js
+++ src/app/(user)/shop/[[...slug]]/page.js
@@ -1,5 +1,19 @@
-function ShopPage({ params }) {
-  return <div>ShopPage</div>;
+"use client";
+
+import { useRouter } from "next/navigation";
+
+function ShopPage() {
+  const router = useRouter();
+  const handleCLick = () => {
+    router.push("/profile");
+  };
+  
+  return (
+    <div>
+      <h1>ShopPage</h1>
+      <button onClick={handleCLick}>place order</button>
+    </div>
+  );
 }
 
 export default ShopPage;
